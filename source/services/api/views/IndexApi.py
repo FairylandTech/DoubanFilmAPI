26411d50498f875553f3cea192294cadc0647d18
AliceEngineer <alice_engineer@yeah.net>
2022-11-14 16:38:30 +0800

    完善接口返回json数据
    Signed-off-by: AliceEngineer <alice_engineer@yeah.net>

--- source/services/api/views/IndexApi.py
+++ source/services/api/views/IndexApi.py
@@ -9,34 +9,41 @@
 from source.services.api import Api
 from flask import jsonify
 from source.services.utils.GetIndexData import GetIndexDataMysql
+from source.services.api.views.PublicApi import PublicApi
 
 
-@Api.route('/get/index/data', methods=['GET'])
+@Api.route('/home', methods=['GET'])
 def get_index_data():
-    result_dict = {
-        'Amount': None,
-        'ScoreMax': None,
-        'ActorMax': None,
-        'MovieCountryMax': None,
-        'MovieTypeAmount': None,
-        'MovieLangMax': None
-    }
-    index_data = GetIndexDataMysql(limit=1000)
-    get_movies_amount = index_data.get_movies_amount()
-    get_movies_score_max = index_data.get_movies_score_max()
-    get_movies_actor_max = index_data.get_movies_actor_max()
-    get_movies_movie_country_max = index_data.get_movies_movie_country_max()
-    get_movies_movie_type_amount = index_data.get_movies_movie_type_amount()
-    get_movies_movie_lang_max = index_data.get_movies_movie_lang_max()
-    result_dict.update(
-        Amount=get_movies_amount,
-        ScoreMax=get_movies_score_max,
-        ActorMax=get_movies_actor_max,
-        MovieCountryMax=get_movies_movie_country_max,
-        MovieTypeAmount=get_movies_movie_type_amount,
-        MovieLangMax=get_movies_movie_lang_max
-    )
-    return jsonify(result_dict)
+    public_api = PublicApi()
+    try:
+        data_dict = {
+            'Amount': None,
+            'ScoreMax': None,
+            'ActorMax': None,
+            'MovieCountryMax': None,
+            'MovieTypeAmount': None,
+            'MovieLangMax': None
+        }
+        index_data = GetIndexDataMysql(limit=1000)
+        get_movies_amount = index_data.get_movies_amount()
+        get_movies_score_max = index_data.get_movies_score_max()
+        get_movies_actor_max = index_data.get_movies_actor_max()
+        get_movies_movie_country_max = index_data.get_movies_movie_country_max()
+        get_movies_movie_type_amount = index_data.get_movies_movie_type_amount()
+        get_movies_movie_lang_max = index_data.get_movies_movie_lang_max()
+        data_dict.update(
+            Amount=get_movies_amount,
+            ScoreMax=get_movies_score_max,
+            ActorMax=get_movies_actor_max,
+            MovieCountryMax=get_movies_movie_country_max,
+            MovieTypeAmount=get_movies_movie_type_amount,
+            MovieLangMax=get_movies_movie_lang_max
+        )
+        public_api.get_build_response_json().update(code=200, msg='Successful', data=data_dict)
+        return jsonify(public_api.get_build_response_json())
+    except Exception as error:
+        print(error)
+        return jsonify(public_api.get_build_response_json())
     
     
     
--- source/services/api/views/PublicApi.py
+++ source/services/api/views/PublicApi.py
@@ -5,3 +5,24 @@
 @HomePage: https://github.com/AliceEngineerPro
 @CreatedTime: 2022/11/11 2:37
 """
+
+class BuildResponseJson(object):
+    result_dict = {
+        'code': 500,
+        'msg': 'Fault',
+        'data': None,
+        'remark': None,
+    }
+
+
+class PublicApi(object):
+    
+    def __init__(self):
+        self.build_response_json = BuildResponseJson() 
+        pass
+    
+    def get_build_response_json(self):
+        return self.build_response_json.result_dict
+        
+
+
